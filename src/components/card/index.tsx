6d7df8b3001cf92f8587c10f14f96679aadfc34b
MattTrovao <matheustrovao@gmail.com>
2023-12-31 14:18:08 -0300

    fix: card search

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -28,7 +28,7 @@ export function Card() {
   const [cardRules, setCardRules] = useState<CardRules | null>(null);
 
   const handleFormSubmit = (data: CardForm) => {
-    setResult(data.result)    
+    setResult(data.result)
 
 
     setCardRules(prevState => ({
@@ -40,120 +40,94 @@ export function Card() {
         return <div key={index}>{rule.comment}</div>;
       }
     }));
-
   };
 
   return (
     <>
       <Container>
         <CardSearch onFormSubmit={handleFormSubmit} />
-      </Container>
 
-      {result !== null ?
-        (
-          <Container>
-            <ResultBox>
-              <NameContainer>
-                {result && <h2>{result.name}</h2>}
+        {result !== null ?
+          (
+            <>
+              <ResultBox>
+                <NameContainer>
+                  {result && <h2>{result.name}</h2>}
 
-                {result?.mana_cost && (
-                  <h2>{result.mana_cost.replace(/{/g, "").replace(/}/g, " ")}</h2>
+                  {result?.mana_cost && (
+                    <h2>{result.mana_cost.replace(/{/g, "").replace(/}/g, " ")}</h2>
+                  )}
+
+                  {result?.card_faces && (
+                    <h2>
+                      {result.card_faces.map((data: CardFace, index: number) => (
+                        <React.Fragment key={index}>
+                          {data.mana_cost.replace(/{/g, "").replace(/}/g, " ")}
+                          {index === 0 && ' // '}
+                        </React.Fragment>
+                      ))}
+                    </h2>
+                  )}
+                </NameContainer>
+
+                {result.image_uris && (
+                  <ResultImgBox>
+                    <ResultImg src={result.image_uris.art_crop} />
+                    <ResultCaption>Artista: <b>{result.artist}</b></ResultCaption>
+                  </ResultImgBox>
                 )}
 
-                {result.card_faces[0]?.mana_cost && (
-                  <h2>
-                    {result.card_faces.map((data: CardFace, index: number) => (
-                      <React.Fragment key={index}>
-                        {data.mana_cost.replace(/{/g, "").replace(/}/g, " ")}
-                        {index == 0 && ' // '}
-                      </React.Fragment>
-                    ))}
-                  </h2>
+                {result?.card_faces && result.card_faces && (
+                  <ResultImgBox className='cardFace'>
+                    <ResultCardFace>
+                      {result.card_faces.map((data: CardFace, index: number) => (
+                        <ResultImg key={index} src={data.image_uris.art_crop} />
+                      ))}
+                    </ResultCardFace>
+                    <ResultCaption>Artista: <b>{result.card_faces[0].artist}</b></ResultCaption>
+                  </ResultImgBox>
                 )}
-              </NameContainer>
 
-              {result.image_uris && (
-                <ResultImgBox>
-                  <ResultImg src={result.image_uris.art_crop} />
-                  <ResultCaption>Artista: <b>{result.artist}</b></ResultCaption>
-                </ResultImgBox>
-              )}
+                <RuleInfo>
+                  <RuleText>
+                    {result.type_line}
+                  </RuleText>
 
-              {result?.card_faces && result.card_faces && (
-                <ResultImgBox className='cardFace'>
-                  <ResultCardFace>
-                    {result.card_faces.map((data: CardFace, index: number) => (
-                      <ResultImg key={index} src={data.image_uris.art_crop} />
-                    ))}
-                  </ResultCardFace>
-                  <ResultCaption>Artista: <b>{result.card_faces[0].artist}</b></ResultCaption>
-                </ResultImgBox>
-              )}
-
-              <RuleInfo>
-                <RuleText>
-                  {result.type_line}
-                </RuleText>
-
-                <RuleText>
-                  {
-                    result.power ?
-                      result.power + '/' + result.toughness
-                      : result.loyalty ?
-                        result.loyalty
-                        : ''
-                  }
-
-                  {
-                    result?.card_faces && result.card_faces &&
-                    result.card_faces.map((data: CardFace, index: number) => (
-                      <React.Fragment key={index}>
-                        {
-                          data.power ?
-                            data.power + '/' + data.toughness
-                            : data.loyalty ?
-                              data.loyalty
-                              : ''
-                        }
-                        {
-                          index == 0 && (data.power || data.loyalty)
-                            ? ' // ' : ''
-                        }
-                      </React.Fragment>
-                    ))
-                  }
-                </RuleText>
-              </RuleInfo>
-
-              {
-                result.oracle_text && (
                   <RuleText>
                     {
-                      result.oracle_text
-                        .replace(/{/g, "")
-                        .replace(/}/g, " ")
-                        .split('\n')
-                        .map((line, index) => (
-                          <React.Fragment key={index}>
-                            {line} <br /><br />
-                          </React.Fragment>
-                        ))
+                      result.power ?
+                        result.power + '/' + result.toughness
+                        : result.loyalty ?
+                          result.loyalty
+                          : ''
+                    }
+
+                    {
+                      result?.card_faces && result.card_faces &&
+                      result.card_faces.map((data: CardFace, index: number) => (
+                        <React.Fragment key={index}>
+                          {
+                            data.power ?
+                              data.power + '/' + data.toughness
+                              : data.loyalty ?
+                                data.loyalty
+                                : ''
+                          }
+                          {
+                            index == 0 && (data.power || data.loyalty)
+                              ? ' // ' : ''
+                          }
+                        </React.Fragment>
+                      ))
                     }
                   </RuleText>
-                )
-              }
-              {result.flavor_text && (
-                <RuleText>
-                  <i>{result.flavor_text}</i>
-                </RuleText>
-              )}
+                </RuleInfo>
 
-              {result?.card_faces && result.card_faces && (
-                <RuleText>
-                  {result.card_faces.map((data: CardFace, index: number) => (
-                    <React.Fragment key={index}>
+                {
+                  result.oracle_text && (
+                    <RuleText>
                       {
-                        data.oracle_text
+                        result.oracle_text
                           .replace(/{/g, "")
                           .replace(/}/g, " ")
                           .split('\n')
@@ -163,52 +137,78 @@ export function Card() {
                             </React.Fragment>
                           ))
                       }
-
-                      {data.flavor_text && (
-                        <i>{data.flavor_text}</i>
-                      )}
-
-                      {index == 0 && <Divider />}
-                    </React.Fragment>
-
-                  ))}
-                </RuleText>
-              )}
-
-
-              {cardRules && cardRules.data && cardRules.data.length > 0 ? (
-                <>
-                  <RuleInfo>
-                    <RuleText>
-                      Regras
                     </RuleText>
-                    <RuleText>
-                      <small>Regras retiradas do <a href={result.scryfall_uri} target='_blank'>Scryfall</a></small>
-                    </RuleText>
-                  </RuleInfo>
-                  <RuleList>
-                    {cardRules.data.map((data: CardRuleData, index: number) => (
-                      <RuleText key={index.toString()}>
-                        <RuleDate>
-                          {formatDate(data.published_at)}:
-                        </RuleDate>
-                        {data.comment}
-                      </RuleText>
+                  )
+                }
+                {result.flavor_text && (
+                  <RuleText>
+                    <i>{result.flavor_text}</i>
+                  </RuleText>
+                )}
+
+                {result?.card_faces && result.card_faces && (
+                  <RuleText>
+                    {result.card_faces.map((data: CardFace, index: number) => (
+                      <React.Fragment key={index}>
+                        {
+                          data.oracle_text
+                            .replace(/{/g, "")
+                            .replace(/}/g, " ")
+                            .split('\n')
+                            .map((line, index) => (
+                              <React.Fragment key={index}>
+                                {line} <br /><br />
+                              </React.Fragment>
+                            ))
+                        }
+
+                        {data.flavor_text && (
+                          <i>{data.flavor_text}</i>
+                        )}
+
+                        {index == 0 && <Divider />}
+                      </React.Fragment>
+
                     ))}
+                  </RuleText>
+                )}
 
 
-                  </RuleList>
-                </>
-              ) : (
-                <NoRule>
-                  Nenhuma regra encontrada
-                </NoRule>
-              )}
+                {cardRules && cardRules.data && cardRules.data.length > 0 ? (
+                  <>
+                    <RuleInfo>
+                      <RuleText>
+                        Regras
+                      </RuleText>
+                      <RuleText>
+                        <small>Regras retiradas do <a href={result.scryfall_uri} target='_blank'>Scryfall</a></small>
+                      </RuleText>
+                    </RuleInfo>
+                    <RuleList>
+                      {cardRules.data.map((data: CardRuleData, index: number) => (
+                        <RuleText key={index.toString()}>
+                          <RuleDate>
+                            {formatDate(data.published_at)}:
+                          </RuleDate>
+                          {data.comment}
+                        </RuleText>
+                      ))}
 
-            </ResultBox>
-          </Container>
-        ) : ''
-      }
+
+                    </RuleList>
+                  </>
+                ) : (
+                  <NoRule>
+                    Nenhuma regra encontrada
+                  </NoRule>
+                )}
+
+              </ResultBox>
+            </>
+          ) : ''
+        }
+      </Container>
+
     </>
   )
 }
