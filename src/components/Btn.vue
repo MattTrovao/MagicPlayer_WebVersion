056765dd700480e0517decbc221108a162c7669f
MattTrovao <matheustrovao@gmail.com>
2025-07-28 10:34:19 -0300

    feat: componente de botão

---
+++ src/components/Btn.vue
@@ -0,0 +1,72 @@
+<script setup>
+const props = defineProps({
+  outline: {
+    type: Boolean,
+    default: false,
+  },
+  secondary: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <button
+    :class="`
+      btn
+      ${props.secondary ? '--secondary' : ''}
+      ${props.outline ? '--outline' : ''}
+    `"
+  >
+    <slot />
+  </button>
+</template>
+
+<style lang="css" scoped>
+.btn {
+  border: 2px solid transparent;
+  border-radius: var(--radius);
+  background: var(--primary-500);
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 10px;
+  font-size: 1.2rem;
+  font-weight: 600;
+  color: var(--white);
+  cursor: pointer;
+  transition: var(--transition);
+
+  &.--outline {
+    border-color: var(--primary-500);
+    background: none;
+    color: var(--primary-500);
+    &:hover {
+      border-color: var(--primary-700);
+      background: var(--primary-200);
+    }
+
+    &.--secondary {
+      border-color: var(--secondary-500);
+      background: none;
+      color: var(--secondary-900);
+      &:hover {
+        border-color: var(--secondary-700);
+        background: var(--secondary-100);
+      }
+    }
+  }
+
+  &.--secondary {
+    background: var(--secondary-500);
+    &:hover {
+      background: var(--secondary-700);
+    }
+  }
+
+  &:hover {
+    background: var(--primary-700);
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import { createApp } from 'vue';
 import App from "./App.vue";
+import './css/main.css';
 import i18n from "./i18n";
 import router from "./router";
-import './style.css';
+
+import "sweetalert2/dist/sweetalert2.min.css";
+import VueSweetalert2 from "vue-sweetalert2";
 
 const app = createApp(App);
 
 
+import Btn from "@/components/Btn.vue";
+app.component("Btn", Btn);
+
+
+
+
 app.use(router);
 app.use(i18n);
+app.use(VueSweetalert2);
 
 app.mount("#app");
